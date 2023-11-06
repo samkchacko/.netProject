d156d0dcb792410bfae52a5678a0837b4a39a892
SamKChacko <chackoksam1@gmail.com>
2023-11-06 08:29:24 +0530

    11/6/2023

--- js/browser.js
+++ js/browser.js
@@ -1,7 +1,13 @@
 window.addEventListener('load',()=>{
-        console.log(navigator.userAgentData)
+        console.log(navigator.geolocation.getCurrentPosition(loc))
         data1.innerHTML=(navigator.userAgent)
-        data2.innerHTML=(navigator.userAgentData.brands[0].brand+navigator.userAgentData.brands[1].brand)
+        data2.innerHTML=(` Browser is ${navigator.userAgentData.brands[0].brand} and brand is ${navigator.userAgentData.brands[1].brand}`)
 })
+loc=(position)=>{
+        data3.innerHTML=`${name.toUpperCase()} your current location is ${position.coords.latitude}Latitude and ${position.coords.longitude}Longitude`
+        
+}
 let data1=document.querySelector('#one')
 let data2=document.querySelector('#two')
+let data3=document.querySelector('#three')
+let name=prompt()
